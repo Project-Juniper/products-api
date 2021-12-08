2d0d8e4ba4fd1adbf681b4b92b2ad26820ab547f
David Deaton <ddeatonv@gmail.com>
2021-12-08 02:53:52 -0800

    Removed comments from schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,3 @@
--- ---
--- Globals
--- ---
-
--- \c products_api
-
--- VARCHAR might take up too much space.
 DROP TABLE IF EXISTS Products CASCADE;
 
 CREATE TABLE Products (
@@ -76,66 +69,3 @@ CREATE INDEX photos_id_idx ON PHOTOS (styles_id);
 CREATE INDEX current_product_id_idx ON Related (current_product_id);
 CREATE INDEX skus_id_idx ON Skus (styles_id);
 CREATE INDEX features_product_idx ON Features (product_id);
--- ---
--- Table 'reviews'
---
--- ---
-
--- DROP TABLE IF EXISTS Reviews CASCADE;
-
--- CREATE TABLE Reviews (
---   id INTEGER GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
---   product_id INTEGER REFERENCES Products(id),
---   reviews_list SET NULL DEFAULT NULL,
---   meta SET NULL DEFAULT NULL
--- );
-
--- ---
--- Table 'questions'
---
--- ---
-
--- DROP TABLE IF EXISTS Questions CASCADE;
-
--- CREATE TABLE Questions (
---   id INTEGER GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
---   product_id INTEGER REFERENCES Products(id),
---   question VARCHAR(255) NULL DEFAULT NULL,
---   answer VARCHAR(255) NULL DEFAULT NULL
--- );
--- ---
--- Table Properties
--- ---
-
--- ALTER TABLE `products` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `styles` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `skus` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `category` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `features` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `photos` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `related_products` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `reviews` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `questions` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- ---
--- Test Data
--- ---
-
--- INSERT INTO `products` (`id`,`name`,`slogan`,`description`,`category_id`,`default_price`,`features_id`,`created_at`,`updated_at`) VALUES
--- ('','','','','','','','','');
--- INSERT INTO `styles` (`id`,`product_id`,`name`,`original_price`,`sale_price`,`default?`) VALUES
--- ('','','','','','');
--- INSERT INTO `skus` (`id`,`quantity`,`size`,`styles_id`) VALUES
--- ('','','','');
--- INSERT INTO `category` (`id`,`name`) VALUES
--- ('','');
--- INSERT INTO `features` (`id`,`feature`,`value`) VALUES
--- ('','','');
--- INSERT INTO `photos` (`id`,`styles_id`,`thumbnail_url`,`url`) VALUES
--- ('','','','');
--- INSERT INTO `related_products` (`id`,`product_id`,`related_product_id`) VALUES
--- ('','','');
--- INSERT INTO `reviews` (`id`,`product_id`,`reviews_list`,`meta`) VALUES
--- ('','','','');
--- INSERT INTO `questions` (`id`,`product_id`,`question`,`answer`) VALUES
--- ('','','','');
